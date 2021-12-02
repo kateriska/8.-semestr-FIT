c1776e407fb79863cac1edb7556dc5feef4febfb
kateriska <43783793+kateriska@users.noreply.github.com>
2021-12-02 20:52:39 +0100

    editted model

--- POVa/du03.py
+++ POVa/du03.py
@@ -6,7 +6,6 @@ import sys
 from tools import collage
 from tools import readCIFAR
 from matplotlib import pyplot as plt
-import tensorflow
 
 # Example showing how to train and use a Convolutional Neural Network in TensorFlow (keras).
 #
@@ -86,23 +85,57 @@ out (Dense)                  (None, 10)                2570
 Total params: 217,466
 Trainable params: 217,466
 Non-trainable params: 0
+
+Model summary:
+Model: "model"
+_________________________________________________________________
+Layer (type)                 Output Shape              Param #
+=================================================================
+input_1 (InputLayer)         [(None, 32, 32, 3)]       0
+_________________________________________________________________
+conv2d (Conv2D)              (None, 30, 30, 8)         224
+_________________________________________________________________
+max_pooling2d (MaxPooling2D) (None, 15, 15, 8)         0
+_________________________________________________________________
+conv2d_1 (Conv2D)            (None, 13, 13, 16)        1168
+_________________________________________________________________
+max_pooling2d_1 (MaxPooling2 (None, 6, 6, 16)          0
+_________________________________________________________________
+flatten (Flatten)            (None, 576)               0
+_________________________________________________________________
+dense (Dense)                (None, 256)               147712
+_________________________________________________________________
+dropout (Dropout)            (None, 256)               0
+_________________________________________________________________
+dense_1 (Dense)              (None, 256)               65792
+_________________________________________________________________
+dropout_1 (Dropout)          (None, 256)               0
+_________________________________________________________________
+dense_2 (Dense)              (None, 10)                2570
+=================================================================
+Total params: 217,466
+Trainable params: 217,466
+Non-trainable params: 0
+
 '''
     from tensorflow.keras.layers import Input, Dense, Dropout, Flatten
     from tensorflow.keras.layers import Conv2D, MaxPooling2D
     from tensorflow.keras.models import Model
 
     # FILL
-    model = tensorflow.keras.Sequential()
-    model.add(Conv2D(filters=8, kernel_size=(3, 3), activation='relu', input_shape=(32,32, 3)))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Conv2D(filters=16, kernel_size=(3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Flatten())
-    model.add(Dense(units=256, activation='relu'))
-    model.add(Dropout(0.15))
-    model.add(Dense(units=256, activation='relu'))
-    model.add(Dropout(0.15))
-    model.add(Dense(units=10, activation = 'softmax'))
+    input = Input(shape=(32,32, 3))
+    layers = Conv2D(filters=8, kernel_size=(3, 3), activation='relu') (input)
+    layers = MaxPooling2D(pool_size=(2, 2)) (layers)
+    layers = Conv2D(filters=16, kernel_size=(3, 3), activation='relu') (layers)
+    layers = MaxPooling2D(pool_size=(2, 2)) (layers)
+    layers = Flatten() (layers)
+    layers = Dense(units=256, activation='relu') (layers)
+    layers = Dropout(0.15) (layers)
+    layers = Dense(units=256, activation='relu') (layers)
+    layers = Dropout(0.15) (layers)
+    layers = Dense(units=10, activation = 'softmax') (layers)
+
+    model = Model(input, layers)
 
     return model
 
@@ -146,8 +179,8 @@ def main():
     # Use SparseCategoricalCrossentropy loss and Adam optimizer with learning rate 0.001.
     # All the imports you need are provided above.
     model.compile(
-        loss= tensorflow.keras.losses.SparseCategoricalCrossentropy(),
-        optimizer= tensorflow.keras.optimizers.Adam(learning_rate=0.001),
+        loss= losses.SparseCategoricalCrossentropy(),
+        optimizer= optimizers.Adam(learning_rate=0.001),
         metrics=[metrics.sparse_categorical_accuracy])
 
     trnData, tstData, trnLabels, tstLabels = prepareData()
@@ -165,7 +198,7 @@ def main():
     # Train the network for 5 epochs on mini-batches of 64 images.
     model.fit(
         x=trnData, y=trnLabels,
-        batch_size=64, epochs=100, verbose=1,
+        batch_size=64, epochs=20, verbose=1,
         validation_data=(tstData, tstLabels), shuffle=True)
 
     # Save the network:
@@ -180,7 +213,8 @@ def main():
     # Compute network (model) responses for dataToTest inputs.
     # This should produce a 2D tensor of the 10 class probabilites for each
     # image in dataToTest. The subsequent code displays the predicted classes.
-    model.load_weights('model.h5')
+
+    #model.load_weights('model.h5')
     classProb = model.predict(dataToTest)# FILL
 
     print('Prediction shape:', classProb.shape)
